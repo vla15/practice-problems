6b5fc197cae6b472eed718c58e6cf438f846645c
Hack Reactor Student <student@hackreactor.com>
2017-05-27 18:15:09 -0700

    returns first non repeated char

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,4 +8,28 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+  //input string
+  //output non repeated char
+  //set counter
+  //iterate through string
+  for (var index = 0; index < string.length; index++) {
+    //counter reset
+    var counter = 0;
+    //take first char as target
+    var target = string[index];
+    //iterate through string again
+    for (var repeat = 0; repeat < string.length; repeat++) {
+      if (target === string[repeat]) {
+        counter++;
+      }
+      // if only appears once
+        //return true
+    }
+    if (counter === 1) {
+      return target;
+    }
+  }
 };
+
+
+firstNonRepeatedCharacter('ABA'); // => 'B'
