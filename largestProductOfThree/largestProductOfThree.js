5516404f5bf3d62ceb3a340eb1d4241080cc2cf3
Vincent La <vincentla@Vincents-MacBook-Pro.local>
2017-05-31 09:56:54 -0700

    working

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -8,65 +8,64 @@
  */
 
 
-var largestProductOfThree = function(array) {
-  //input array
-  //output product
-  //store largest values
-  var productTotal = undefined;
-  var product = function(value, index) {
+// var largestProductOfThree = function(array) {
+//   //input array
+//   //output product
+//   //store largest values
+//   var productTotal = undefined;
+//   var product = function(value, index) {
 
-    var thirdProduct = function(value, firIndex, secIndex) {
-      for (var thirdIndex = 0; thirdIndex < array.length; thirdIndex++) {
-        if (thirdIndex !== firIndex || thirdIndex !== secIndex) {
-          var finalTotal = value * array[thirdIndex];
-          console.log(finalTotal);
-          if (productTotal === undefined) {
-            productTotal = finalTotal;
-          } else if (finalTotal > productTotal) {
-            productTotal = finalTotal;
-          }
-        }
-      }
-    }
+//     var thirdProduct = function(value, firIndex, secIndex) {
+//       for (var thirdIndex = 0; thirdIndex < array.length; thirdIndex++) {
+//         if (thirdIndex !== firIndex || thirdIndex !== secIndex) {
+//           var finalTotal = value * array[thirdIndex];
+//           if (productTotal === undefined) {
+//             productTotal = finalTotal;
+//           } else if (finalTotal > productTotal) {
+//             productTotal = finalTotal;
+//           }
+//         }
+//       }
+//     }
 
-    for (var secIndex = 0; secIndex < array.length; secIndex++) {
-      if (index !== secIndex) {
-        var total = value * array[secIndex];
-        thirdProduct(total, index, secIndex)
-      }
-    }
+//     for (var secIndex = 0; secIndex < array.length; secIndex++) {
+//       if (index !== secIndex) {
+//         var total = value * array[secIndex];
+//         thirdProduct(total, index, secIndex)
+//       }
+//     }
 
-  }
-  for (var index = 0; index < array.length; index++) {
-    product(array[index], index);
-  }
-  return productTotal;
-}
+//   }
+//   for (var index = 0; index < array.length; index++) {
+//     product(array[index], index);
+//   }
+//   return productTotal;
+// }
 
-largestProductOfThree([1, 2, 3])
+// largestProductOfThree([1, 2, 3])
   //iterate through array with first value constant
 
 
-// var largestProductOfThree = function(array) {
-//   //sort array so last three numbers in back
-//   if (array.length <= 3) {
-//     return array.reduce(function(num1, num2) {
-//       return num1 * num2;
-//     })
-//   }
-//   // modify array sort
-//   var sorted = array.sort(function(num1, num2) {
-//     return num1 - num2;
-//   });
-//   console.log(sorted);
-//   // create new array with last three largest numbers
-//   var lastThree = sorted.slice(sorted.length - 3);
-//   // return product of new array
-//   return lastThree.reduce(function(num1, num2) {
-//     return num1 * num2;
-//   })
-//   // TODO: everything
-// };
+var largestProductOfThree = function(array) {
+  //sort array so last three numbers in back
+  if (array.length <= 3) {
+    return array.reduce(function(num1, num2) {
+      return num1 * num2;
+    })
+  }
+  // modify array sort
+  var sorted = array.sort(function(num1, num2) {
+    return num1 - num2;
+  });
+  console.log(sorted);
+  // create new array with last three largest numbers
+  var lastThree = sorted.slice(sorted.length - 3);
+  // return product of new array
+  return lastThree.reduce(function(num1, num2) {
+    return num1 * num2;
+  })
+  // TODO: everything
+};
 
 // console.log(largestProductOfThree([-3, -2, -1, -4, 6]));
 
