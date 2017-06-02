87ffb16e40e75c2accf2d30b6560c52e7fa01eef
Vincent La <vincentla@Vincents-MacBook-Pro.local>
2017-06-02 09:37:24 -0700

    Passes two deepEquality tests

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -12,4 +12,33 @@
   *
   */
 var deepEquals = function(apple, orange) {
+  //input objects
+  //output boolean
+
+  //boolean set to true
+  var isEquivalent = true;
+  //iterate through object
+  for (var key in apple) {
+    // check if keys match
+      // match then check if object
+        // if both values are objects then recurse
+        // else compare values
+      // no match then false
+    if (orange.hasOwnProperty(key)) {
+      if (typeof apple[key] === 'object' && typeof orange[key] === 'object') {
+        isEquivalent = deepEquals(apple[key], orange[key])
+      } else if (apple[key] !== orange[key]) {
+        isEquivalent = false;
+      }
+    } else {
+      isEquivalent = false;
+    }
+  }
+    // check if value is an object
+      // recurse
+
+  return isEquivalent;
 };
+
+console.log(deepEquals({a:1, b: {c:3}},{a:1, b: {c:3}})); // true
+console.log(deepEquals({a:1, b: {c:5}},{a:1, b: {c:6}})); // false
