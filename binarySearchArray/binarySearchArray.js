3dd73fd963325c820146235d1aac2883ddd7877e
Vincent La <vincentla@Vincents-MacBook-Pro.local>
2017-06-06 13:20:54 -0700

    refactored code to eliminate case of middle being equal to start or end

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,39 +11,32 @@
 var binarySearch = function (array, target) {
   var start = arguments[2] || 0;
   var end = arguments[3] || array.length;
-  console.log(start, end);
   // get middle value of arr with start + end
-  var middle = Math.floor((start + end) / 2);
+  var middle = Math.floor((end + start) / 2);
   // check if middel value equals target
   if (array[middle] === target) {
     return middle;
     // return index
-  } else if (array[start] === target) {
-    return start;
-  } else if (array[end] === target) {
-    return end
   }
   // if middle equals to start or end
   // basecase
-  if (middle === start || middle === end) {
+  if (start === end) {
     return null;
     // return false
   }
   // placeholder for start index
   // placeholder for end index
   // check if target is smaller than middle
-  if (target < array[middle]) {
+  if (array[middle] > target) {
     // recurse with array from start to middle -1
-    end = middle - 1;
-    return binarySearch(array, target, start, end);
+    return binarySearch(array, target, start, middle - 1);
   }
-  if (target > array[middle]) {
-    start = middle + 1;
-    return binarySearch(array, target, start, end);
+  if (array[middle] < target) {
+    return binarySearch(array, target, middle + 1, end);
   }
   // check if target is larger than middle
     // recurse with array from end to middle +1
 };
 
-var index = binarySearch([1, 2, 3, 4, 5], 2)
-console.log(index);
+// var index = binarySearch([1, 2, 3, 4, 5], 2)
+// console.log(index);
