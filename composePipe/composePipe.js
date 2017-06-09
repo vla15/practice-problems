e33e4cab0bf942b269e71569d2281e408e63c77a
Vincent La <vincentla@Vincents-MacBook-Pro.local>
2017-06-09 09:43:05 -0700

    adds a pipe function that accepts x number of function arguments and applies function calls left to right on a value;

--- composePipe/composePipe.js
+++ composePipe/composePipe.js
@@ -39,8 +39,6 @@ var compose = function() {
   //shoudl return a function
   var calls = arguments
   return function(target) {
-    debugger;
-    var result;
     for (var index = calls.length - 1; index >= 0; index--) {
       target = calls[index](target);
     }
@@ -53,11 +51,31 @@ var compose = function() {
 
 
 
-var greet = function(name){ return 'hi: ' + name;}
-var exclaim = function(statement) { return statement.toUpperCase() + '!';}
-var welcome = compose(greet, exclaim);
+// var greet = function(name){ return 'hi: ' + name;}
+// var exclaim = function(statement) { return statement.toUpperCase() + '!';}
+// var welcome = compose(greet, exclaim);
 
-console.log(welcome('phillip'));
+// console.log(welcome('phillip'));
 
 var pipe = function() {
+  //input: x number of functions in arguments
+  var calls = arguments;
+  // returns a function
+  return function(target) {
+    debugger;
+    // iterates left to to right on arguments
+    for (var index = 0; index < calls.length; index++) {
+      // set target to the result of the function calls
+      target = calls[index](target);
+    }
+    // return target
+    return target;
+  }
 };
+
+
+// var add2 = function(number){ return number + 2; }
+// var multiplyBy3 = function(number){ return number * 3; }
+// console.log(pipe(add2, multiplyBy3)(5)) // 21
+// console.log(pipe(add2, multiplyBy3, multiplyBy3)(5)) // 63
+
