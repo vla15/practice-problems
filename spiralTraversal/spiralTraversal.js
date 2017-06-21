cc4028d246309720917c51c8b57ceff4b2bba71f
Vincent La <vincentla@Vincents-MacBook-Pro.local>
2017-06-21 09:38:12 -0700

    able to spiral a 3x3 matrix

--- spiralTraversal/spiralTraversal.js
+++ spiralTraversal/spiralTraversal.js
@@ -15,6 +15,72 @@
  */
 
 var spiralTraversal = function(matrix) {
-
-  // TODO: Implement me!
+  //input: array of arrays
+  // output singular array of spiraled values
+  // create results array
+  var results = [];
+  // while length of matrix is > 0
+  while (matrix.length > 0) {
+    // need to traverse the first sub array
+    var first = matrix.splice(0, 1);
+    first = concatArray(first);
+      //splice first array
+    for (var firIndex = 0; firIndex < first.length; firIndex++) {
+      // iterate through first array forward and adding values to result
+      results.push(first[firIndex]);
+    }
+    if (matrix.length > 0) {
+      // then traverse the main array for all end values
+      for (var mainIndex = 0; mainIndex < matrix.length; mainIndex++) {
+        var lastIndexValue = matrix[mainIndex].splice(matrix[mainIndex].length - 1, 1);
+        // splice values from sub arrays
+        results.push(lastIndexValue);
+        // add to results
+      }
+      // then traverse the last sub array in reverse
+      // check if there's a last
+      var last = matrix.splice(matrix.length - 1, 1);
+      last = concatArray(last);
+      for (var lastIndex = last.length - 1; lastIndex >= 0; lastIndex--) {
+        results.push(last[lastIndex]);
+      }
+      // splice values from sub array
+      // add to results
+    // then traverse main array backwards for first values
+      //splice last array
+      // iterate through last array backwards and add values to result
+    // check if there are any arrays left
+      if (matrix.length > 0) {
+        for (var firstIndex = 0; firstIndex < matrix.length; firstIndex++) {
+          var firstIndexValue = matrix[firstIndex].splice(0, 1);
+          results.push(firstIndexValue);
+        }
+      }
+    // then traverse second sub array
+    // TODO: Implement me!
+          
+    }
+  }
+  return concatArray(results);
 };
+
+var concatArray = function(arr) {
+  return arr.reduce((array, value) => array.concat(value), [])
+}
+
+
+var assertEqual = function(actual, expected, test) {
+  if (actual === expected) {
+    console.log('success!');
+  } else {
+    console.log(`FAILED TEST [${test}] got ${actual} when expected: ${expected}`);
+  }
+}
+
+var results = spiralTraversal([
+      [1,2,3],
+      [4,5,6],
+      [7,8,9]
+    ]);
+
+console.log(results);
