ab8d7d2682c32613e2bad193b07d1d923c311ac1
Vincent La <vincentla@Vincents-MacBook-Pro.local>
2017-07-14 09:12:45 -0700

    Completed prime tester

--- primeTester/primeTester.js
+++ primeTester/primeTester.js
@@ -5,12 +5,29 @@
  */
 
 var primeTester = function(n) {
-  if (typeof n !== 'number' || n < 1 || n % 1 !== 0) {
+  if (typeof n !== 'number' || n < 1 || n % 1 !== 0 || n === 1) {
     // n isn't a number or n is less than 1 or n is not an integer
     return false;
   }
+  // create counter at 2
+  var counter = 2;
+  // edge case: 1
+  while (counter <= n - 1) {
+    // iterate through from 2 to n - 1
+    if (n % counter === 0) {
+      return false;
+    }
+    counter++;
+    // if any result is 0
+      // return false
+    // return true
+    
+  }
+  return true;
   // TODO: return true if n is prime, false otherwise
 };
+
+console.log(primeTester(20));
 
 /* Extra credit: Write a function that generates a list of all prime numbers
  * in a user-specified range (inclusive). If you're not quite sure where to start,
