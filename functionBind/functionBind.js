36050850ccc296dc028ad91a39db4e8a467557e5
Vincent La <vincentla@Vincents-MacBook-Pro.local>
2017-07-25 09:56:54 -0700

    attempted function bind

--- functionBind/functionBind.js
+++ functionBind/functionBind.js
@@ -23,10 +23,20 @@
  *
 */
 
-var bind = function(
-) {
+var bind = function(a, b, c) {
   // TODO: Your code here
+  // func can take three values
+  // returns a function
+  return function(z, b) {
+    return c + z;
+  }
+
 };
+
+//has to pass arguments and this down to then return function in bind.
+// var func = function(a, b){ return a + b };
+// var boundFunc = bind(func, null, 'foo');
+// var result = boundFunc('bar');
 
 /*
  * Function.prototype.bind:
@@ -53,7 +63,21 @@ var bind = function(
  *
 */
 
-Function.prototype.bind = function(
-) {
+var alice = {
+  name: 'alice',
+  shout: function () {
+    console.log(this.name);
+  }
+}
+
+Function.prototype.bind = function(...args) {
+  //returns the original funtion
+  //changes the this binding
+  var bound = function () {
+  }
+  return Object.assign(bound, arguments);
+  //changes the this binding to the passed in value
   // TODO: Your code here
 };
+
+var boundShout = alice.shout.bind(alice);
