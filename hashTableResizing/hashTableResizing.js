a3a3caa40373737a940affa859ba602aed3a20a5
Vincent La <vincentla@Vincents-MacBook-Pro.local>
2017-07-29 09:25:56 -0700

    rebuilt remove insert and retrieve hash functions

--- hashTableResizing/hashTableResizing.js
+++ hashTableResizing/hashTableResizing.js
@@ -26,20 +26,67 @@ var makeHashTable = function() {
   var storageLimit = 4;
   var size = 0;
   
-  result.insert = function(/*...*/ 
-) {
+  result.insert = function(key, value) {
+    //hash the key
+    var hash = getIndexBelowMaxForKey(key, storageLimit);
+    //locate hash of storage
+    storage[hash] = storage[hash] || [];
+    var wasInserted = false;
+    for (var index = 0; index < storage[hash].length; index++) {
+      if (storage[hash][index][0] === key) {
+        storage[hash][index][1] = value;
+        wasInserted = true;
+      }
+    }
+
+    if (!wasInserted) {
+      storage[hash].push([key, value]);
+    }
+      // if no array then insert array
+    // iterate through array
+      // if no key match
+        //push to array
+      // replace key match value with new value
     // TODO: implement `insert`
   };
 
-  result.retrieve = function(/*...*/ 
-) {
+  result.retrieve = function(key) {
     // TODO: implement `retrieve`
+    // hash key again
+    var hash = getIndexBelowMaxForKey(key, storageLimit)
+    // locate targetStorage
+    var targetStorage = storage[hash];
+    for (var index = 0; index < targetStorage.length; index++) {
+      if (targetStorage[index][0] === key) {
+        return targetStorage[index][1];
+      }
+    }
+    return null;
+    // iterate through targetStorage
+      // if key matches tuple[0]
+        //return value
+    // return null
   };
 
-  result.remove = function(/*...*/ 
-) {
+  result.remove = function(key) {
     // TODO: implement `remove`
+    var hash = getIndexBelowMaxForKey(key, storageLimit);
+
+    var targetStorage = storage[hash];
+
+    for (var index = 0; index < targetStorage.length; index++) {
+      if (targetStorage[index][0] === key) {
+        var removed = targetStorage.splice(index, 1);
+      }
+    }
+    return removed
   };
 
   return result;
 };
+
+var testHash = new makeHashTable();
+console.log(testHash.insert('blah', 'champion'));
+
+console.log(testHash.retrieve('blah'));
+console.log(testHash.remove('blah'));
