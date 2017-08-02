3016a672e2a30221f4af6ea2384a8536e8925646
Vincent La <vincentla@Vincents-MacBook-Pro.local>
2017-08-02 09:37:25 -0700

    sorting functionality

--- sortableTable/sortableTable.js
+++ sortableTable/sortableTable.js
@@ -10,6 +10,25 @@
 
 $(function () {
   // TODO: your code here!
+  $('th').click(function() {
+    var clickedHeaderIndex = event.target.cellIndex
+    var row = $('tbody').children();
+    var sortedArr = [];
+    for (var index = 0; index < row.length; index++) {
+      sortedArr.push(row[index].children[clickedHeaderIndex].innerHTML)
+      //push values into an array
+      //sort
+
+    }
+    sortedArr = sortedArr.sort();
+    for (var i = 0; i < row.length; i++) {
+      row[i].children[clickedHeaderIndex].innerHTML = sortedArr[i];
+    }
+  })
+
+  //record click event
+  //check if click occurred on a table header
+  //check all rows in header.  Will be based on index
 
 });
 
