40a27dd6c95b0d13c524ab716b52cf81e16a8fae
Vincent La <vincela15@gmail.com>
2017-08-14 19:29:52 -0700

    completed anagrams

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -13,5 +13,35 @@
   */
 
 var allAnagrams = function(string) {
-  // Your code here.
+  //create a results storage
+  //start with each letter of string
+  var results = {};
+
+  const anagram = function(ana, str) {
+    if (str === '') {
+      results[ana] = 1;
+    }
+    for (var i = 0; i < str.length; i++) {
+      console.log(str);
+      anagram(ana + str[i], str.slice(0, i) + str.slice(i + 1));
+    }
+  }
+  anagram('', string);
+  return Object.keys(results);
+  //go down each possible combination
+    //when string result is same length
+      //push to results storage
 };
+
+const assertEqual = (expected, actual) => {
+  if (JSON.stringify(expected) === JSON.stringify(actual)) {
+    console.log('equals!');
+  } else {
+    console.log(`failed, expected ${expected}, but got ${actual}`);
+  }
+}
+
+var test = allAnagrams('abc');
+
+assertEqual(['abc', 'acb', 'bac', 'bca', 'cab', 'cba'], test);
+
