19ed21d869fcb4bb155815e33b325e82fb6f194b
Vincent La <vincela15@gmail.com>
2017-08-17 15:30:09 -0700

    commenting insertion sort

--- sortPractice/sort.js
+++ sortPractice/sort.js
@@ -65,5 +65,55 @@ const mergeSort = (arr) => {
 
 let mergeSortTest = mergeSort([5, 9, 2, 3, 5, 4, 3, 1])
 assertEqual([1, 2, 3, 3, 4, 5, 5, 9], mergeSortTest);
+
+
 //insertionSort
+
+const insertionSort = (arr) => {
+  //insertion sort
+  //estasblishes a baseline starting point
+  for (var index = 1; index < arr.length; index++) {
+    var target = arr[index];
+    //goes backwards on the sorted section
+    //compares last value of sorted section to make sure its the largest
+    //if its not then swap
+      //because you're swapping you need to compare rest of sorted section as its no longer sorted anymore
+    for (var j = index - 1; j >= 0 && arr[j] > target; j--) {
+      arr[j + 1] = arr[j];
+    }
+    //breakpoint
+    //if there was swapping then j is updated to where the target is no longer the smallest
+    //sets that value at that point to be target;
+    arr[j + 1] = target;
+  }
+  return arr;
+  //check every number after that
+  //then proceed backwards to sort in ascending order
+  //loop stops, replace value
+}
+
+console.log(insertionSort([4, 5, 2, 1]));
+
 //bubble
+const swap = function(arr, i, j) {
+  var temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+const bubbleSort = (arr) => {
+  let isSorted = false;
+  while (!isSorted) { 
+    for (var index = 0; index < arr.length; index++) {
+      if (arr[index + 1] < arr[index]) {
+        swap(arr, index + 1, index);
+        isSorted = true;
+      }
+    }
+    isSorted = !isSorted;
+  }
+  return arr;
+}
+
+let bubbleTest = bubbleSort([5, 9, 2, 1, 4]);
+assertEqual([1, 2, 4, 5, 9], bubbleTest);
