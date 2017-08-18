23f49933632898ac38f7f5f4fa6938d9034844c1
Vincent La <vincela15@gmail.com>
2017-08-18 14:44:00 -0700

    reimplemeneted mergeSort and insertionSort

--- sortPractice/sort.js
+++ sortPractice/sort.js
@@ -28,6 +28,62 @@ const bubbleSort = (arr) => {
 }
 
 console.log(bubbleSort([23, 4, 5, 9, 0 , 2]));
+
 //insertionsort
+
+const insertionSort = (arr) => {
+  //iterate through arr
+  for (var i = 0; i < arr.length; i++) {
+    var cur = arr[i];
+    for (var j  = i - 1; j >= 0 && cur < arr[j]; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = cur;
+  }
+    //take current value
+    //compare against sorted part of arr
+      //if end of sorted is greater
+        //swap
+    //insert curr value to sorted arr
+  //return arr
+  return arr;
+}
+
+console.log(insertionSort([4, 2, 15, 9, 3, 5, 7]));
 //mergesort
+const merge = (left, right) => {
+  let merged = [];
+  let il = 0;
+  let ir = 0;
+
+  while (il < left.length && ir < right.length) {
+    if (left[il] < right[ir]) {
+      merged.push(left[il++]);
+    } else {
+      merged.push(right[ir++]);
+    }
+  }
+
+  if (il === left.length) {
+    return merged.concat(right.slice(ir));
+  } else {
+    return merged.concat(left.slice(il));
+  }
+}
+
+
+
+const mergeSort = (arr) => {
+  if (arr.length < 2) {
+    return arr;
+  }
+
+  let mid = Math.floor(arr.length / 2);
+  let left = arr.slice(0, mid);
+  let right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
 //quicksort
+
+console.log(mergeSort([4, 7, 9, 2, 8, 12]));
