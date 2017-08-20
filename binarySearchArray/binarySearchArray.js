d0349b0d8149961c728817e85fdbf5bb1a25d964
Vincent La <vincela15@gmail.com>
2017-08-20 12:42:31 -0700

    solved binary search array

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -8,6 +8,28 @@
  * console.log(index); // 3
  */
 
-var binarySearch = function (array, target) {
+var binarySearch = function (array, target, left, right) {
 
+  var left = left || 0;
+  var right = right || array.length - 1;
+
+  var mid = Math.floor((left + right)/ 2);
+  if (array[mid] === target) {
+    return mid;
+  }
+
+  if (target < array[mid]) {
+    return binarySearch(array, target, left, mid - 1)
+  } else {
+    return binarySearch(array, target, mid + 1, right);
+  }
+
+  return null;
+  //split array
+  //check which side of array target falls under
+  //recurse
 };
+
+console.log(binarySearch([1, 2, 3, 4, 5], 5));
+
+//5 / 2 2.5 mid 2
