fc7f602df04a602419ce21675ba74f172995cbb3
Vincent La <vincela15@gmail.com>
2017-08-19 22:36:18 -0700

    added bubble sort

--- sortPractice/sort.js
+++ sortPractice/sort.js
@@ -1,5 +1,22 @@
 //bubbleSort
+const bubbleSort = (arr) => {
+  //track sorting
+  let wasSorted = false;
+  while (!wasSorted) {
+    for (var index = 0; index < arr.length; index++) {
+      if (arr[index + 1] < arr[index]) {
+        var temp = arr[index];
+        arr[index] = arr[index + 1];
+        arr[index + 1] = temp;
+        wasSorted = true;
+      }
+    }
+    wasSorted = !wasSorted;
+  }
+  return arr;
+}
 
+console.log(bubbleSort([4, 9, 123, 3, 0, 1, 19]))
 //insertionSort
 //quickSort
 //mergeSort
