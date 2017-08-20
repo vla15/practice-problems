3e8fad5c529d1bcc78caf179e8d743afad37874a
Vincent La <vincela15@gmail.com>
2017-08-19 22:43:56 -0700

    implemented insertion sort

--- sortPractice/sort.js
+++ sortPractice/sort.js
@@ -16,7 +16,27 @@ const bubbleSort = (arr) => {
   return arr;
 }
 
-console.log(bubbleSort([4, 9, 123, 3, 0, 1, 19]))
+console.log(bubbleSort([4, 9, 123, 3, 0, 1, 19]));
 //insertionSort
+
+const insertionSort = (arr) => {
+  //iterate through initial array
+  for (var i = 0; i < arr.length; i++) {
+    var currentValue = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > currentValue; j--) {
+      arr[j + 1] = arr[j]
+    }
+    arr[j + 1] = currentValue;
+  }
+  return arr;
+  //check every value in array against next value
+  //if next value is smaller
+    //iterate in reverse the sorted part of arr
+    //continue until target value not smaller
+  //replace value at current index with target value
+  //return arr
+}
+
+console.log(insertionSort([14, 4, 9, 3, 2, 8, 1]));
 //quickSort
 //mergeSort
