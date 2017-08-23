019bb75dc0bef96f742c25315dcdaa390d8c4d50
Vincent La <vincela15@gmail.com>
2017-08-23 15:41:06 -0700

    completed char frequency

--- characterFrequency/characterFrequency.js
+++ characterFrequency/characterFrequency.js
@@ -37,10 +37,36 @@
 
 
 var characterFrequency = function(string) {
+  //create a ledger
+  var results = [];
+  //iterate throught the string
+  for (var i = 0; i < string.length; i++) {
+    var incremented = false
+    for (var j = 0; j < results.length; j++) {
+      if (results[j][0] === string[i]) {
+        results[j][1]++;
+        incremented = true;
+      }
+    }
+    if (!incremented) {
+      results.push([string[i], 1]);
+    }
+  }
+  return results.sort((a, b) => {
+    if (b[1] > a[1]) {
+      return 1;
+    } else if (a[0] > b[0]) {
+      return 0;
+    }
+  })
+  //count each occurrance
+  //return sorted by desc order and then asc char
 };
 
-
-// console.log(characterFrequency('aaabbc'));
-// console.log(characterFrequency('mississippi'));
 // console.log(characterFrequency('miaaiaaippi'));
-// console.log(characterFrequency('mmmaaaiiibbb'));
+
+
+console.log(characterFrequency('aaabbc'));
+console.log(characterFrequency('mississippi'));
+console.log(characterFrequency('miaaiaaippi'));
+console.log(characterFrequency('mmmaaaiiibbb'));
