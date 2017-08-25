23a482d124ed1e8cf3152d15edf72b0d2cd9f047
Vincent La <vincela15@gmail.com>
2017-08-25 08:11:57 -0700

    insertion sort implemented

--- sortPractice/sort.js
+++ sortPractice/sort.js
@@ -1,5 +1,21 @@
 //insertionSort
-
+const insertionSort = (arr) => {
+  //itreate through array
+  for (var i = 0; i < arr.length; i++) {
+    var target = arr[i];
+    //this for loop checks the last value of the sorted arrays
+    for (var j = i - 1; j >= 0 && arr[j] > target; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = target;
+  }
+  return arr;
+  //place sorted values to left of arr
+  //if we find not sorted
+  //have to go back left side arr
+  //check values
+}
+console.log(insertionSort([8, 3, 5, 2, 9, 7, 1]))
 //bubblesort
 const bubbleSort = (arr) => {
   var isSorted = false;
@@ -18,6 +34,7 @@ const bubbleSort = (arr) => {
 }
 
 console.log(bubbleSort([8, 3, 5, 2, 9, 7, 1]))
+
 //iterates through array
 //checks if its sorted
 //if not 
