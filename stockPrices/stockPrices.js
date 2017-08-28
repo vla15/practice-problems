752a6c990d1a26cac2b8f052499f4ea3c7d1c28e
Vincent La <vincela15@gmail.com>
2017-08-28 15:43:33 -0700

    optimized for linear big o notation

--- stockPrices/stockPrices.js
+++ stockPrices/stockPrices.js
@@ -8,30 +8,52 @@ var negativePricesYesterday = [-10, -7, -5, -8, -11, -9];
 // must buy before selling
 
 const getMaxProfit = function(prices) {
-  //output is largest difference
-  // find the max difference between cur value and cur + 2
-  //brute force method
-  //var to hold maxProfit
+  //keep track of min price
+  //initialize max profit
   var maxProfit;
-  for (var i = 0 ; i < prices.length; i++) {
-    for (var j = i + 2; j < prices.length; j++) {
-      if (i !== j && prices[j]) {
-        var diff = prices[j] - prices[i];
-        if (!maxProfit) {
-          maxProfit = diff;
-        } else {
-          maxProfit = Math.max(maxProfit, diff);
-        }
-      }
+  if (prices.length < 2) {
+    return null;
+  } else {
+    maxProfit = prices[0] - prices[1];
+  }
+
+  var minPurchase = prices[0];
+  for (var i = 2; i < prices.length; i++) {
+    if (prices[i] < minPurchase) {
+      minPurchase = prices[i]
+    } else {
+      maxProfit = Math.max(maxProfit, prices[i] - minPurchase);
     }
   }
-  return maxProfit
-  //iterate through array
-    //take cur value
-    //get difference for profit
-    //compare if larger
-      //update maxProfit
+  return maxProfit;
 }
 
-console.log(getMaxProfit(stockPricesYesterday));
+
+// const getMaxProfit = function(prices) {
+//   //output is largest difference
+//   // find the max difference between cur value and cur + 2
+//   //brute force method
+//   //var to hold maxProfit
+//   var maxProfit;
+//   for (var i = 0 ; i < prices.length; i++) {
+//     for (var j = i + 2; j < prices.length; j++) {
+//       if (i !== j && prices[j]) {
+//         var diff = prices[j] - prices[i];
+//         if (!maxProfit) {
+//           maxProfit = diff;
+//         } else {
+//           maxProfit = Math.max(maxProfit, diff);
+//         }
+//       }
+//     }
+//   }
+//   return maxProfit
+//   //iterate through array
+//     //take cur value
+//     //get difference for profit
+//     //compare if larger
+//       //update maxProfit
+// }
+
+// console.log(getMaxProfit(stockPricesYesterday));
 console.log(getMaxProfit(negativePricesYesterday));
