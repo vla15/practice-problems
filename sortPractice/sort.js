82fa9d405e0bedf1e0e429fe4ddef4d46a7a4093
Vincent La <vincela15@gmail.com>
2017-08-28 22:55:34 -0700

    redid sorting practices.

--- sortPractice/sort.js
+++ sortPractice/sort.js
@@ -1,23 +1,21 @@
-//insertionSort
-const insertionSort = (arr) => {
-  //itreate through array
+//insertion sort
+const insertionSort = function(arr) {
+  //iterate through
   for (var i = 0; i < arr.length; i++) {
     var target = arr[i];
-    //this for loop checks the last value of the sorted arrays
     for (var j = i - 1; j >= 0 && arr[j] > target; j--) {
       arr[j + 1] = arr[j];
     }
     arr[j + 1] = target;
   }
   return arr;
-  //place sorted values to left of arr
-  //if we find not sorted
-  //have to go back left side arr
-  //check values
+  //place sorted values to left of iteration
 }
-console.log(insertionSort([8, 3, 5, 2, 9, 7, 1]))
-//bubblesort
-const bubbleSort = (arr) => {
+
+console.log(insertionSort([9, 2, 5, 3, 5, 4]))
+//bubble sort
+
+const bubbleSort = function(arr) {
   var isSorted = false;
   while (!isSorted) {
     for (var i = 0; i < arr.length; i++) {
@@ -30,57 +28,14 @@ const bubbleSort = (arr) => {
     }
     isSorted = !isSorted;
   }
-  return arr
+  return arr;
 }
 
-console.log(bubbleSort([8, 3, 5, 2, 9, 7, 1]))
-
-//iterates through array
-//checks if its sorted
-//if not 
-//reiterate through array
-//mergeSort
-//merge helper function
-//split values of array into individual values
-//merge them back in order
-const merge = (left, right) => {
-  let il = 0;
-  let ir = 0;
-  let results = [];
-  while (il < left.length && ir < right.length) {
-    if (left[il] < right[ir]) {
-      results.push(left[il++]);
-    } else {
-      results.push(right[ir++]);
-    }
-  }
-  if (il === left.length) {
-    return results.concat(right.slice(ir));
-  } else {
-    return results.concat(left.slice(il));
-  }
-}
-
-console.log(merge([5, 9], [2, 10]));
-
-const mergeSort = (arr) => {
+console.log(bubbleSort([9, 2, 5, 3, 5, 4]))
+//quick sort
+const quickSort = function(arr) {
   if (arr.length < 2) {
     return arr;
-  }
-
-  var mid = Math.floor(arr.length / 2);
-  var left = arr.slice(0, mid);
-  var right = arr.slice(mid);
-
-  return merge(mergeSort(left), mergeSort(right));
-}
-
-console.log(mergeSort([8, 3, 5, 2, 9, 7, 1]))
-//quickSort
-
-const quickSort = (arr) => {
-  if (arr.length < 2) {
-    return arr
   }
 
   var pivot = arr[0];
@@ -98,4 +53,56 @@ const quickSort = (arr) => {
   return quickSort(left).concat(pivot, quickSort(right));
 }
 
-console.log(quickSort([8, 3, 5, 2, 9, 7, 1]))
+console.log(quickSort([9, 2, 5, 3, 5, 4]))
+//picks a pivot point
+//spilt array into left and right buckets
+//recurse on left and right side
+//till one value in each array and return array
+//merge sort
+
+///merge sort has a helper function that returns a whole array sorted;
+//takes in two sorted arrays and combines them into a single sorted array
+
+const merge = function(left, right) {
+  var il = 0;
+  var ir = 0;
+  var results = [];
+
+  while (il < left.length && ir < right.length) {
+    if (left[il] < right[ir]) {
+      results.push(left[il++]);
+    } else {
+      results.push(right[ir++]);
+    }
+  }
+
+  if (il === left.length) {
+    return results.concat(right.slice(ir));
+  } else {
+    return results.concat(left.slice(il));
+  }
+}
+
+console.log(merge([4, 6], [7, 8, 9]));
+
+const mergeSort = function(arr) {
+  //takes a single array
+  if (arr.length < 2) {
+    return arr;
+  }
+
+  var mid = Math.floor(arr.length / 2);
+  var left = arr.slice(0, mid);
+  var right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+  //spilts it into left and right until theres only 1 value left
+  //merges those values back in order
+
+}
+
+console.log(mergeSort([9, 2, 5, 3, 5, 4]));
+
+
+
+
