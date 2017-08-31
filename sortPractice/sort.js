0e3355d1949bc7a0e783255e9a702d5b8a206ff1
Vincent La <vincela15@gmail.com>
2017-08-30 22:48:02 -0700

    adding in quickSort

--- sortPractice/sort.js
+++ sortPractice/sort.js
@@ -34,4 +34,27 @@ const bubbleSort = function(arr) {
 
 console.log(bubbleSort([8, 4, 2, 9, 5, 7, 6]))
 //quick sort
+
+
+const quickSort = function(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+
+  var pivot = arr[0];
+  var left = [];
+  var right = [];
+
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+
+  return quickSort(left).concat(pivot, quickSort(right));
+}
+
+console.log(quickSort([8, 4, 2, 9, 5, 7, 6]))
 //merge sort
