175e662cdff38a61042855a4a4b01a2294cec645
Vincent La <vincela15@gmail.com>
2017-09-06 23:08:05 -0700

    completed largest product of three in linear time

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -10,6 +10,46 @@
 
 
 var largestProductOfThree = function(array) {
-  //sort array so last three numbers in back
+  //keep track of three maxes
+  //keep track of two mins
+  var thirdMax = Number.NEGATIVE_INFINITY;
+  var secondMax = Number.NEGATIVE_INFINITY;
+  var firstMax = Number.NEGATIVE_INFINITY;
+  var firstMin = Number.POSITIVE_INFINITY;
+  var secondMin = Number.POSITIVE_INFINITY;
+  for (var i = 0; i < array.length; i++) {
+    var minTarget = array[i];
+    if (minTarget < secondMin) {
+      var tmp = secondMin;
+      secondMin = minTarget;
+      minTarget = tmp;
+    }
+    if (minTarget < firstMin) {
+      tmp = firstMin;
+      firstMin = minTarget;
+      minTarget = tmp
+    }
+    var maxTarget = array[i];
+    if (maxTarget > thirdMax) {
+      tmp = thirdMax;
+      thirdMax = maxTarget;
+      maxTarget = tmp;
+    }
+    if (maxTarget > secondMax) {
+      tmp = secondMax;
+      secondMax = maxTarget;
+      maxTarget = tmp;
+    }
+    if (maxTarget > firstMax) {
+      tmp = firstMax;
+      firstMax = maxTarget;
+      maxTarget = tmp;
+    }
+  }
+  return thirdMax * Math.max(secondMax * firstMax, firstMin * secondMin);
 };
+
+console.log(largestProductOfThree([-4, -3, -2, -1, 60]));
+
+
 
