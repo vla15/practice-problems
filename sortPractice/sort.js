6a2a30768d750a3266cf2728c149bbe1779be96e
Vincent La <vincela15@gmail.com>
2017-09-08 22:48:37 -0700

    insertionSort reimplemented

--- sortPractice/sort.js
+++ sortPractice/sort.js
@@ -1,3 +1,4 @@
+//insertionSort
 const insertionSort = function(arr) {
   for (var i = 0; i < arr.length; i++) {
     var target = arr[i];
@@ -9,80 +10,8 @@ const insertionSort = function(arr) {
   return arr;
 }
 
-console.log(insertionSort([9, 4, 2, 5, 8, 3, 7]));
+console.log(insertionSort([4, 9, 7, 2, 5, 6, 1]));
 
-const bubbleSort = function(arr) {
-  var sorted = false;
-  while (!sorted) {
-    for (var i = 0; i < arr.length; i++) {
-      if (arr[i] > arr[i + 1]) {
-        var tmp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = tmp;
-        sorted = true;
-      }
-    }
-    sorted = !sorted;
-  }
-  return arr;
-}
-
-console.log(bubbleSort([9, 4, 2, 5, 8, 3, 7]));
-
-const quickSort = function(arr) {
-  if (arr.length < 2) {
-    return arr;
-  }
-
-  var pivot = arr[0];
-  var left = [];
-  var right = [];
-
-  for (var i = 1; i < arr.length; i++) {
-    if (arr[i] < pivot) {
-      left.push(arr[i]);
-    } else {
-      right.push(arr[i]);
-    }
-  }
-  return quickSort(left).concat(pivot, quickSort(right));
-}
-
-console.log(quickSort([9, 4, 2, 5, 8, 3, 7]));
-
-
-const merge = function(left, right) {
-  var il = 0;
-  var ir = 0;
-  var results = [];
-
-  while (il < left.length && ir < right.length) {
-    if (left[il] < right[ir]) {
-      results.push(left[il++]);
-    } else {
-      results.push(right[ir++]);
-    }
-  }
-
-  if (il === left.length) {
-    return results.concat(right.slice(ir));
-  } else {
-    return results.concat(left.slice(il));
-  }
-}
-
-console.log(merge([4, 9], [2, 7]));
-
-const mergeSort = function(arr) {
-  if (arr.length < 2) {
-    return arr;
-  }
-
-  var mid = Math.floor(arr.length / 2);
-  var left = arr.slice(0, mid);
-  var right = arr.slice(mid);
-
-  return merge(mergeSort(left), mergeSort(right));
-}
-
-console.log(mergeSort([9, 4, 2, 5, 8, 3, 7]));
+//bubbleSort
+//quickSort
+//mergeSort
