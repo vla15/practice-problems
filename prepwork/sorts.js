7b16fed8805c0bffe7d5178c26464ba3f3988081
Vincent La <vincent.la@nokia.com>
2018-02-23 15:44:05 -0800

    reworking merge sort

--- prepwork/sorts.js
+++ prepwork/sorts.js
@@ -1,29 +1,34 @@
 //merge sort
-const mergeSort = function (array) {
-  //spilt array into two
-  //get mid point
+//spilt array into two arrays
+//spilt until array is of one size and compare
+const mergeSort = function(array) {
   if (array.length < 2) {
-    return array;
+    return array
   }
   let mid = Math.floor(array.length / 2);
-  //iterate array
-  //smaller than numbers than mid go left
-  //greater than numbers than mid go right
-  let left = [];
-  let right = [];
-  for (var i = 0; i < array.length; i++) {
-    if (i !== mid) {
-      if (array[i] < array[mid]) {
-        left.push(array[i]);
-      } else {
-        right.push(array[i]);
-      }
-    }
-  }
-  return mergeSort(left).concat(array[mid]).concat(mergeSort(right));
+  let left = array.slice(0, mid);
+  let right = array.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
 }
 
-console.log(mergeSort([3, 2, 1, 5, 7]))
+//takes all values of left and right and orders them
+const merge = function(left, right) {
+  let results = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      results.push(left[leftIndex++]);
+    } else {
+      results.push(right[rightIndex++]);
+    }
+  }
+  return results.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+console.log(mergeSort([17, 2, 5, 23, 1, 4, 0]));
 
 //[3, 2, 1, 5, 7];
 // [] [1] [2, 3, 5, 7]
