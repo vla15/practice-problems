48b9513df5c30f9e296f6cbe656ee0c73a795123
Vincent La <vincent.la@nokia.com>
2018-02-24 21:12:51 -0800

    reimplementing sorts

--- prepwork/sorts.js
+++ prepwork/sorts.js
@@ -1,100 +1,33 @@
 //mergesort
+const mergeSort = function(array) {}
+const merge = function(array) {}
 //insertionSort
-const insertionSortTWO = function(array) {
-  //start at one after beginning
-  if (array.length < 2) {
-    return array;
-  }
-
+const insertionSort = function(array) {
+  //starts at index + 1
+  //examines if current value is greater than previous value
   for (var i = 1; i < array.length; i++) {
-    var tmpValue = array[i];
-    for (var j = i - 1; j >= 0 && array[j] > tmpValue; j--) {
+    let curValue = array[i];
+    for (var j = i - 1; j >= 0 && array[j] > curValue; j--) {
       array[j + 1] = array[j];
     }
-    array[j + 1] = tmpValue;
+    array[j + 1] = curValue;
   }
   return array;
+    //moves backwards to check if current value needs to be moved back further
+  //swaps value
 }
-console.log('is two', insertionSortTWO([3, 1, 3, 4, 5, 2, 9]));
-//bubbleSort
 //quickSort
-const quickSortTWO = function(array) {
-  if (array.length < 2) {
-    return array;
-  }
-  let pivot = array[0];
-  let lesser = [];
-  let greater = [];
-
-  for (var i = 1; i < array.length; i++) {
-    if (array[i] < pivot) {
-      lesser.push(array[i]);
-    } else {
-      greater.push(array[i]);
-    }
-  }
-
-  return quickSortTWO(lesser).concat(pivot, quickSortTWO(greater));
-}
-console.log('qs two', quickSortTWO([4, 2, 31, 2, 1]))
+const quickSort = function (array) {}
 //countingSort
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//merge sort
-//O(n log n); iterates through the entire array but spilts array in half
-const mergeSort = function(array) {
-  if (array.length < 2) {
-    return array
-  }
-  let mid = Math.floor(array.length / 2);
-  let left = array.slice(0, mid);
-  let right = array.slice(mid);
-
-  return merge(mergeSort(left), mergeSort(right));
-}
-
-//takes all values of left and right and orders them
-const merge = function(left, right) {
-  let results = [];
-  let leftIndex = 0;
-  let rightIndex = 0;
-
-  while (leftIndex < left.length && rightIndex < right.length) {
-    if (left[leftIndex] < right[rightIndex]) {
-      results.push(left[leftIndex++]);
-    } else {
-      results.push(right[rightIndex++]);
-    }
-  }
-  return results.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
-}
-
-console.log('mergeSort: ', mergeSort([17, 2, 5, 23, 1, 4, 0]));
-//bubble sort
+const countingSort = function (array) {}
+//bubbleSort
 
 const bubbleSort = function(array) {
   let swapped = false;
   while (!swapped) {
-    for (var i = 0; i < array.length; i++) {
+    for (var i = 0; i < array.length; i ++) {
       if (array[i] > array[i + 1]) {
+        //swap
         let tmp = array[i];
         array[i] = array[i + 1];
         array[i + 1] = tmp;
@@ -106,81 +39,146 @@ const bubbleSort = function(array) {
   return array;
 }
 
-console.log('bubble sort: ', bubbleSort([2, 9, 3, 4]));
 
 
-//quick sort
-//O(n log n)
-const quickSort = function(array) {
-  if (array.length < 2) {
-    return array;
-  }
-  let pivot = array[0];
-  let left = [];
-  let right = [];
-  for (var i = 1; i < array.length; i++) {
-    if (array[i] < pivot) {
-      left.push(array[i]);
-    } else {
-      right.push(array[i])
-    }
-  }
-  return quickSort(left).concat(pivot, quickSort(right))
-}
-console.log('quick sort:', quickSort([3, 1, 2, 12, 15,4 ]))
 
-//insertion sort
-const insertionSort = function(array) {
-  if (array.length < 2) {
-    return array;
-  }
-  for (var i = 1; i < array.length; i++) {
-    var value = array[i];
-    //iterates from one index back of i to 0
-    //shifts back all values
-    for (var j = i - 1; j >= 0 && array[j] > value; j--) {
-      //swap next value with current
-      array[j + 1] = array[j];
-    }
-    //places the value in correct spot
-    array[j + 1] = value;
-  }
-  return array;
-}
-console.log('insertion sort: ',insertionSort([42, 1, 4, 52, 3]));
-console.log('insertion sort: ', insertionSort([2, 3, 4, 5, 1]))
 
-//insertion sort approaches the problem by iterating backwards only if the current value at index is less then the value at the previous index.  It will continue until it finds the spot to place the current value at.
 
-//selection sort
 
-//bucket sort
 
-//counting sort
-//time efficient but very space inefficient
-const countingSort = function(array) {
-  let numOfCounts = [];
-  let results = [];
-  //create a new array that tracks count of values in input array
-  //value of the input array will represent the index of new array
-  for (var i = 0; i < array.length; i++) {
-    if (numOfCounts[array[i]]) {
-      numOfCounts[array[i]]++;
-    } else {
-      numOfCounts[array[i]] = 1;
-    }
-  }
-  numOfCounts.forEach(function(counts, number) {
-    if (numOfCounts[number]) {
-      for (var x = 0; x < counts; x++) {
-        results.push(number);
-      }
-    }
-  })
-  return results;
-}
 
-console.log('counting sort: ', countingSort([2, 1, 3, 4, 6, 5, 1]));
+
+
+
+
+
+
+
+
+
+// //merge sort
+// //O(n log n); iterates through the entire array but spilts array in half
+// const mergeSort = function(array) {
+//   if (array.length < 2) {
+//     return array
+//   }
+//   let mid = Math.floor(array.length / 2);
+//   let left = array.slice(0, mid);
+//   let right = array.slice(mid);
+
+//   return merge(mergeSort(left), mergeSort(right));
+// }
+
+// //takes all values of left and right and orders them
+// const merge = function(left, right) {
+//   let results = [];
+//   let leftIndex = 0;
+//   let rightIndex = 0;
+
+//   while (leftIndex < left.length && rightIndex < right.length) {
+//     if (left[leftIndex] < right[rightIndex]) {
+//       results.push(left[leftIndex++]);
+//     } else {
+//       results.push(right[rightIndex++]);
+//     }
+//   }
+//   return results.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+// }
+
+// console.log('mergeSort: ', mergeSort([17, 2, 5, 23, 1, 4, 0]));
+// //bubble sort
+
+// const bubbleSort = function(array) {
+//   let swapped = false;
+//   while (!swapped) {
+//     for (var i = 0; i < array.length; i++) {
+//       if (array[i] > array[i + 1]) {
+//         let tmp = array[i];
+//         array[i] = array[i + 1];
+//         array[i + 1] = tmp;
+//         swapped = true;
+//       }
+//     }
+//     swapped = !swapped;
+//   }
+//   return array;
+// }
+
+// console.log('bubble sort: ', bubbleSort([2, 9, 3, 4]));
+
+
+// //quick sort
+// //O(n log n)
+// const quickSort = function(array) {
+//   if (array.length < 2) {
+//     return array;
+//   }
+//   let pivot = array[0];
+//   let left = [];
+//   let right = [];
+//   for (var i = 1; i < array.length; i++) {
+//     if (array[i] < pivot) {
+//       left.push(array[i]);
+//     } else {
+//       right.push(array[i])
+//     }
+//   }
+//   return quickSort(left).concat(pivot, quickSort(right))
+// }
+// console.log('quick sort:', quickSort([3, 1, 2, 12, 15,4 ]))
+
+// //insertion sort
+// const insertionSort = function(array) {
+//   if (array.length < 2) {
+//     return array;
+//   }
+//   for (var i = 1; i < array.length; i++) {
+//     var value = array[i];
+//     //iterates from one index back of i to 0
+//     //shifts back all values
+//     for (var j = i - 1; j >= 0 && array[j] > value; j--) {
+//       //swap next value with current
+//       array[j + 1] = array[j];
+//     }
+//     //places the value in correct spot
+//     array[j + 1] = value;
+//   }
+//   return array;
+// }
+// console.log('insertion sort: ',insertionSort([42, 1, 4, 52, 3]));
+// console.log('insertion sort: ', insertionSort([2, 3, 4, 5, 1]))
+
+// //insertion sort approaches the problem by iterating backwards only if the current value at index is less then the value at the previous index.  It will continue until it finds the spot to place the current value at.
+
+// //selection sort
+
+// //bucket sort
+
+// //counting sort
+// //time efficient but very space inefficient
+// const countingSort = function(array) {
+//   let numOfCounts = [];
+//   let results = [];
+//   //create a new array that tracks count of values in input array
+//   //value of the input array will represent the index of new array
+//   for (var i = 0; i < array.length; i++) {
+//     if (numOfCounts[array[i]]) {
+//       numOfCounts[array[i]]++;
+//     } else {
+//       numOfCounts[array[i]] = 1;
+//     }
+//   }
+//   numOfCounts.forEach(function(counts, number) {
+//     if (numOfCounts[number]) {
+//       for (var x = 0; x < counts; x++) {
+//         results.push(number);
+//       }
+//     }
+//   })
+//   return results;
+// }
+
+// console.log('counting sort: ', countingSort([2, 1, 3, 4, 6, 5, 1]));
 
 module.exports = {
   mergeSort: mergeSort,
--- prepwork/sorts.spec.js
+++ prepwork/sorts.spec.js
@@ -1,3 +1,7 @@
 const sort = require('./sorts');
 
-console.log(sort.bubbleSort([2, 8, 1, 3, 4, 6, 2]));
+console.log('bubbleSort: ', sort.bubbleSort([2, 8, 1, 3, 4, 6, 2]));
+console.log("countingSort: ", sort.countingSort([2, 8, 1, 3, 4, 6, 2]));
+console.log("insertionSort: ", sort.insertionSort([2, 8, 1, 3, 4, 6, 2]));
+console.log("mergeSort: ", sort.mergeSort([2, 8, 1, 3, 4, 6, 2]));
+console.log("quickSort: ", sort.quickSort([2, 8, 1, 3, 4, 6, 2]));
