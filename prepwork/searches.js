f31c6790009bd07ebcc85a30b14fe3c510107b1c
Vincent La <vincent.la@nokia.com>
2018-02-26 11:03:47 -0800

    redoing tests

--- prepwork/searches.js
+++ prepwork/searches.js
@@ -25,3 +25,63 @@ console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8], 5));
   //return
 //breath first search
 //depth first search
+Object.prototype.depthFirstSearch = function(target) {
+  //takes a tree
+  let idx;
+  if (this.value === target) {
+    return this.index;
+  }
+  //checks if value is at tree
+    //return index/id
+  if (this.left) {
+    idx = idx || this.depthFirstSearch.call(this.left, target);
+  }
+  if (this.right) {
+    idx = idx || this.depthFirstSearch.call(this.right, target);
+  }
+  return idx;
+  //takes left leaf
+    //recurse down leaf
+  //takes right leaf
+    //recurse down leaf
+  //return null
+}
+
+let binaryTree = {
+  value: 5,
+  index: 0,
+  left: {
+    value: 2,
+    index: 1,
+    left: {
+      value: 3,
+      index: 3,
+      left: null,
+      right: null
+    },
+    right: {
+      value: 7,
+      index: 4,
+      left: null,
+      right: null
+    }
+  },
+  right: {
+    value: 6,
+    index: 2,
+    left: {
+      value: 4,
+      index: 5,
+      left: null,
+      right: null
+    },
+    right: {
+      value: 10,
+      index: 6,
+      left: null,
+      right: null
+    }
+  }
+}
+
+console.log(binaryTree.depthFirstSearch(15))
