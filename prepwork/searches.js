75fe9a18b6bfff6515f5789ac9923ee1ac55ab85
Vincent La <vincent.la@nokia.com>
2018-02-27 22:57:07 -0800

    added bfs dfs and binary search

--- prepwork/searches.js
+++ prepwork/searches.js
@@ -1,6 +1,121 @@
 //binarySearch
+const binarySearch = function(arr, target, left = 0, right = arr.length) {
+  //terminating case, when left boundary is greater than right
+  if (left > right) {
+    return null;
+  }
+  //takes mid point
+  let mid = Math.floor((left + right) / 2);
+  //sees if mid point equals target
+  if (target === arr[mid]) {
+    return mid;
+  } else if (target < arr[mid]) {
+    return binarySearch(arr, target, left, mid - 1);
+  } else if (target > arr[mid]) {
+    return binarySearch(arr, target, mid + 1, right);
+  }
+  //then checks if target is less than midpoint
+    //recurse with right boundary as midpoint -1
+  //checks if target is greater than midpoint
+    //recurse with left boundary as midpoint + 1
+}
+console.log(binarySearch([2, 3, 4, 5, 6, 7], 5));
 //depthFirstSearch
+const dfs = function(tree, target) {
+  let left = null;
+  let right = null;
+  if (tree.value === target) {
+    return tree.index;
+  }
+  if (tree.left) {
+    left = dfs(tree.left, target);
+  }
+  if (tree.right) {
+    right = dfs(tree.right, target);
+  }
+  return left || right;
+}
+
+let testTree = {
+  value: 0,
+  index: 0,
+  left: {
+    value: 1,
+    index: 1,
+    left: {
+      value: 6,
+      index: 3,
+      left: {},
+      right: {}
+    },
+    right: {
+      value: 4,
+      index: 4,
+      left: {},
+      right: {}
+    }
+  },
+  right: {
+    value: 2,
+    index: 2,
+    left: {
+      value: 8,
+      index: 5,
+      left: {},
+      right: {}
+    },
+    right: {
+      value: 8,
+      index: 6,
+      left: {},
+      right: {}
+    }
+  }
+}
+console.log(dfs(testTree, 6))
+//depth first iterative
+const dfsIterative = function (root, target) {
+  let stack = [root];
+  while (stack.length > 0) {
+    let node = stack.pop();
+    if (node.value === target) {
+      return node.index;
+    }
+    if (node.right) {
+      stack.push(node.right)
+    }
+    if (node.left) {
+      stack.push(node.left);
+    }
+  }
+}
+
+console.log('dfs iterative', dfsIterative(testTree, 6))
+//iterate would be greating a stack
+//set root to first value in stack
+//continue to iterate through stack while values in stack
+//pop off stack
+//add right leaf if it exists
+//add left leaf if it exists
 //breathFirstSearch
+//creates a queueing system for breathfirst searches
+const bfs = function(root, target) {
+  let queue = [root];
+  while (queue.length > 0) {
+    let node = queue.shift();
+    if (node.value === target) {
+      return node.index;
+    }
+    if (node.left) {
+      queue.push(node.left);
+    }
+    if (node.right) {
+      queue.push(node.right);
+    }
+  }
+}
+
+console.log(bfs(testTree, 8));
 
 
 
